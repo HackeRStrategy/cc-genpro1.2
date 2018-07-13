1c6e6700dc36b8e6344a72a5511d1d7a042615be
RealStrategy <36258164+RealStrategy@users.noreply.github.com>
2018-07-12 21:58:54 -0500

    Add files via upload

---
+++ paquetes.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+#Estte script es necesario para que funcione #cc-genpro1.2"
+#Descargando paquetes
+apt-get update; apt-get upgrade -y ;apt-get install python2 -y; apt-get install w3m -y
+apt-get install git -y
