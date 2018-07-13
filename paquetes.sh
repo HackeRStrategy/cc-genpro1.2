88eed05f0e0d212493bab9096d0fe4839d073983
RealStrategy <36258164+RealStrategy@users.noreply.github.com>
2018-07-12 22:03:58 -0500

    Delete paquetes.sh

--- paquetes.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-
-#Estte script es necesario para que funcione #cc-genpro1.2"
-#Descargando paquetes
-apt-get update; apt-get upgrade -y ;apt-get install python2 -y; apt-get install w3m -y
-apt-get install git -y
