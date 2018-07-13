e53b4335867afb2e6610a1d9bcdedf186d2b0d3c
RealStrategy <36258164+RealStrategy@users.noreply.github.com>
2018-07-13 07:48:32 -0500

    Update paquetes.sh

--- paquetes.sh
+++ paquetes.sh
@@ -2,7 +2,8 @@
 
 #Este script es necesario para que funcione #cc-genpro1.2
 #Descargando paquetes
-apt update
-apt upgrade -y
-pkg install python2 -y
-pkg install w3m -y
+apt update && apt upgrade -y
+
+pkg install python2
+pkg install w3m
+pkg install neofetch
