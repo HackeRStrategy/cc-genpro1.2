f98f155de4deb062da1143ddb984de7673e8141c
barba99 <38580791+barba99@users.noreply.github.com>
2018-08-13 09:50:08 -0500

    Update cc-genpro1.2.py

--- cc-genpro1.2.py
+++ cc-genpro1.2.py
@@ -5,26 +5,26 @@ import os
 import time
 # Version
 os.system("clear")
-print ("\033[1;33mCREATED BY: Real Strategy\033[0m")
-print ("\033[1;32mVERSION: cc-genpro1.2 \033[0m")
+print ("\033[1;33mEDITADO POR JORGE BARBA\033[0;35m")
+print ("\033[1;33mVERSION: generadorcc1.2 \033[0;35m")
 os.system("neofetch")
 print ("")
 ## Show menu ##
 print (30 * '-') 
-print ("\033[1;32mGENERADOR DE BIN RS\033[0m") 
+print ("\033[1;33mGENERADOR BARBA\033[0m") 
 print (30 * '-') 
-print ("\033[1;33m1.\033[1;36mGenerar bin\033[0m") 
-print ("\033[1;33m2.\033[1;36mGuardar bins en texto\033[0m") 
-print ("\033[1;33m3.\033[1;36mGenerador de correo temporal\033[0m")
-print ("\033[1;33m4.\033[1;36mBin Checker\033[0m")
-print ("\033[1;33m5.\033[1;36mVer bin guardados\033[0m")
-print ("\033[1;33m6.\033[1;36mCreditos al creador\033[0m")
-print ("\033[1;33m7.\033[1;36mInformacion\033[0m")
-print ("\033[1;33m8.\033[1;31mSalir...!!\033[0m")
+print ("\033[1;31m1.\033[0;32mGenerar cc\033[0m") 
+print ("\033[1;31m2.\033[0;32mGuardar bins en texto\033[0m") 
+print ("\033[1;31m3.\033[0;32mGenerador de correo temporal\033[0m")
+print ("\033[1;31m4.\033[0;32mBin Checker\033[0m")
+print ("\033[1;31m5.\033[0;32mVer bin guardados\033[0m")
+print ("\033[1;31m6.\033[0;32mCreditos al creador\033[0m")
+print ("\033[1;31m7.\033[0;32mInformacion\033[0m")
+print ("\033[1;31m8.\033[0;32mSalir...!!\033[0m")
 print (30 * '-')
  
 ## Get input ###
-choice = raw_input('\033[1;33mingresa tu opcion: \033[0m')
+choice = raw_input('\033[1;31mingresa tu opcion: \033[0m')
  
 ### Convert string to int type ##
 choice = int(choice)
@@ -32,7 +32,7 @@ choice = int(choice)
 ### Take action as per selected menu-option ###
 if choice == 1:
         print ("Generador bin validos...") 
-        os.system("python2 cc-genpro.py")
+        os.system("python2 generadorcc.py")
         time.sleep(3)
 
 elif choice == 2:
@@ -55,7 +55,7 @@ elif choice == 6:
          os.system("clear")
          os.system("bash creditos")
          time.sleep(8)
-         os.system("python2 cc-genpro1.2.py")
+         os.system("python2 generadorcc.py")
 elif choice == 7:
          print ("")
          print ("MENSAJE: Este es un simple script para generar bins y ahorrar tiempo al momento de binear en el navegador.")
